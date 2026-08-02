be4d730acadf21a1f4dd3721e068ec94c0b23452
oscar1-max <akanegbuosinachi@gmail.com>
2026-08-02 13:36:29 +0100

    Add image property to FeaturedProducts data

--- components/FeaturedProducts.tsx
+++ components/FeaturedProducts.tsx
@@ -6,24 +6,28 @@ const products = [
     name: "Wireless Headphones",
     price: "$89",
     rating: 4.8,
+    image: "/images/headphones.jpg",
   },
   {
     id: 2,
     name: "Smart Watch",
     price: "$149",
     rating: 4.7,
+    image: "/images/watch.jpg",
   },
   {
     id: 3,
     name: "Gaming Mouse",
     price: "$59",
     rating: 4.9,
+    image: "/images/mouse.jpg",
   },
   {
     id: 4,
     name: "Leather Backpack",
     price: "$99",
     rating: 4.6,
+    image: "/images/backpack.jpg",
   },
 ];
 
@@ -42,6 +46,7 @@ export default function FeaturedProducts() {
               name={product.name}
               price={product.price}
               rating={product.rating}
+              image={product.image}
             />
           ))}
         </div>
