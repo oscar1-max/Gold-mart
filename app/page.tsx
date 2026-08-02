3b7415520081354ae338bd55a8e0bcc85f72daf1
oscar1-max <akanegbuosinachi@gmail.com>
2026-08-02 13:18:17 +0100

    Add featured products to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Categories from "@/components/Categories";
+import FeaturedProducts from "@/components/FeaturedProducts";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Categories />
+      <FeaturedProducts />
     </>
   );
 }
