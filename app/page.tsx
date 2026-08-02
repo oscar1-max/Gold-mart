007ecf655ace9e9004f7c4814aaa6e6e56ab2fb7
oscar1-max <akanegbuosinachi@gmail.com>
2026-08-02 13:24:07 +0100

    Add Footer component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Categories from "@/components/Categories";
 import FeaturedProducts from "@/components/FeaturedProducts";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Categories />
       <FeaturedProducts />
+      <Footer />
     </>
   );
 }
